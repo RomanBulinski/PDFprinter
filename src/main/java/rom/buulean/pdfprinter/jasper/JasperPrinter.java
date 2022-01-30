5c1fc367e32556a72bebfae9f0db51cf5b9bdb95
bulir <aboandrus@gamil.com>
2022-01-30 17:37:28 +0100

    clean code

--- src/main/java/rom/buulean/pdfprinter/jasper/JasperPrinter.java
+++ src/main/java/rom/buulean/pdfprinter/jasper/JasperPrinter.java
@@ -1,9 +1,10 @@
 package rom.buulean.pdfprinter.jasper;
 
+import lombok.AllArgsConstructor;
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.engine.util.JRSaver;
-
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
@@ -11,32 +12,48 @@ import net.sf.jasperreports.export.SimplePdfReportConfiguration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import rom.buulean.pdfprinter.workers.application.WorkersService;
+import rom.buulean.pdfprinter.workers.model.Worker;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+@AllArgsConstructor
 @Service
 public class JasperPrinter {
 
-    public JasperPrinter()  {}
+    private final WorkersService workersService;
 
-    public byte[] getBytePdfReport(){
+    public byte[] getBytePdfReport() {
 
         byte[] report = null;
-        Resource reportResource = new ClassPathResource("reports/names2234.jrxml");
         try {
+            //Kompilacja *.jrxml -> *.jasper
+            Resource reportResource = new ClassPathResource("reports/pracownicy.jrxml");
             InputStream reportStream = reportResource.getInputStream();
             JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
+            //Mozna zapisac plik jasper lokalnie
+            JRSaver.saveObject(jasperReport, "temporaryJasper.jasper");
 
+            //Przygotowanie Paramatrów do wstrzyknięcia
             Map<String, Object> parameters = new HashMap<>();
-            parameters.put("title1", "Employee Report");
-            ;
-            JasperPrint jasperPrint = JasperFillManager.fillReport(
-                    jasperReport, parameters, new JREmptyDataSource() );
+            parameters.put("company", "SofNet");
+            //Przygotowanie Danych do wstrzyknięcia
+            List<Worker> workers = workersService.findAllWorkers();
+            JRDataSource dataSource = new JRBeanCollectionDataSource(workers, true);
 
-            report =  JasperExportManager.exportReportToPdf(jasperPrint);
+            //Plik JasperReport(*.jasper) wypelnieny danymi i zwracany jako JasperPrint
+            JasperPrint jasperPrint = JasperFillManager.fillReport(
+                    jasperReport,
+                    parameters,
+                    dataSource);
+//            new JREmptyDataSource());
+
+            //Plik JasperPrint -> byte[] w  formacie PDF
+            report = JasperExportManager.exportReportToPdf(jasperPrint);
 
         } catch (IOException | JRException e) {
             e.printStackTrace();
@@ -68,8 +85,6 @@ public class JasperPrinter {
 
         exporter.exportReport();
     }
-
-
 
 
 }
